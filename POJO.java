317ab0203be517fade2b50893b3d15bb28d9eae6
Kike <kike.jurado2000@gmail.com>
2019-02-06 13:40:43 +0100

    Commit del POJO

---
+++ POJO.java
@@ -0,0 +1,19 @@
+
+public class POJO{
+public static void POJO main(String[] args) { 
+        int pojo1;
+        int pojo2;
+        int pojototal;
+        pojo1 = 3;
+        pojo2 = 4;
+        pojototal=pojo1+pojo2;
+        System.out.println("La suma de los pojos es "+pojototal);
+}
+}
+
+
+
+
+
+
+
